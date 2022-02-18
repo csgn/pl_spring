56d7a7d684ed1c0fb13364cec6d20374fd1b3ba9
csgn <dev.csgn@gmail.com>
2022-02-18 16:52:18 +0300

    Extraction

--- odev1/odev.c
+++ odev1/odev.c
@@ -11,97 +11,126 @@ Sergen Cepoglu
 
 #define SIZE INT_MAX
 
-int parseInt(char n)
-{
-    return n - '0';
+int parseInt(char n) {
+	return n - '0';
 }
 
-int parseChar(int n)
-{
-    return n + '0';
+int parseChar(int n) {
+	return n + '0';
 }
 
-void Reverse(char* str)
-{
-    int len = strlen(str);
-    int i = 0;
-    while (len > i)
-    {
-        char tmp = str[--len];
-        str[len] = str[i];
-        str[i++] = tmp;
-    }
+void Reverse(char* str) {
+	int len = strlen(str);
+	int i = 0;
+	while (len > i) {
+		char tmp = str[--len];
+		str[len] = str[i];
+		str[i++] = tmp;
+	}
+}
+
+void Show(char *fn, char* sn, char* res) {
+	int fnl, snl, resl;
+	int x, i;
+	fnl = strlen(fn);
+	snl = strlen(sn);
+	resl = strlen(res);
+
+	for (i = 0; i < fnl; i++)
+		printf("%c", fn[i]);
+	printf("\n");
+
+	x = (fnl - snl)*2;
+	while (x-- > 0)
+		printf(" ");
+
+	for (i = 0; i < snl; i++)
+		printf("%c", sn[i]);
+
+	printf("\n");
+	for (i = 0; i < resl; i++)
+		printf("%c", res[i]);
+}
+
+char* Addition(char* fn, char* sn) {
+	int c = 0, r = 0;
+	int i = strlen(fn)-1, j = strlen(sn)-1, k = 0;
+	int fni, sni;
+	char* res = (char*) malloc(sizeof(char)*SIZE);
+
+	while (i >= 0 || j >= 0 ) {
+		sni = j < 0 ? 0 : parseInt(sn[j--]);
+		fni = i < 0 ? 0 : parseInt(fn[i--]);
+		c = fni + sni + r;
+		r = c / 10;
+		res[k++] = parseChar(c%10);
+	}
+
+	Reverse(res);
+	return res;
+}
+
+char* Extraction(char* fn, char* sn) {
+	int c = 0, r = 0;
+	int i = strlen(fn)-1, j = strlen(sn)-1, k = 0;
+	int fni, sni;
+	char* res = (char*) malloc(sizeof(char)*SIZE);
+
+	char* cpyfn = (char*) malloc(sizeof(char)*SIZE);
+	memcpy(cpyfn, fn, sizeof(char)*SIZE);
+
+	while (i >= 0 || j >= 0 ) {
+		sni = j < 0 ? 0 : parseInt(sn[j]);
+		fni = i < 0 ? 0 : parseInt(cpyfn[i]);
+
+		if (fni < sni) {
+			int tmp = i-1;
+
+			while (parseInt(cpyfn[tmp]) == 0) {
+				tmp--;
+			}
+
+			cpyfn[tmp] = parseChar(parseInt(cpyfn[tmp]) - 1);
+
+			while (++tmp < i) {
+				if (parseInt(cpyfn[tmp]) == 0) {
+					cpyfn[tmp] = parseChar(9);
+				}
+			}
+
+			int t = parseInt(cpyfn[tmp])+10 - sni;
+			res[k] = parseChar(t);
+		} else {
+			res[k] = parseChar(fni-sni);
+		}
+
+		i--;
+		j--;
+		k++;
+	}
+
+	Reverse(res);
+	return res;
+}
+
+char* Multiplication(char* fn, char* sn) {
+
 }
 
 
-char* Addition(char* fn, char* sn)
-{
-    int c = 0, r = 0;
-    int i = strlen(fn)-1, j = strlen(sn)-1, k = 0;
-    int fni, sni;
-    char* res = (char*) malloc(sizeof(char)*SIZE);
-
-    while (i >= 0 || j >= 0 )
-    {
-        sni = j < 0 ? 0 : parseInt(sn[j--]);
-        fni = i < 0 ? 0 : parseInt(fn[i--]);
-        c = fni + sni + r;
-        r = c / 10;
-        res[k++] = parseChar(c%10);
-    }
-
-    Reverse(res);
-    return res;
-}
-
-char* Multiplication(char* fn, char* sn)
-{
-
-}
-
-char* Extraction(char* fn, char* sn)
-{
-
-}
-
-void Show(char *fn, char* sn, char* res)
-{
-    int fnl, snl, resl;
-    int x, i;
-    fnl = strlen(fn);
-    snl = strlen(sn);
-    resl = strlen(res);
-
-    for (i = 0; i < fnl; i++)
-        printf("%c ", fn[i]);
-    printf("\n");
-
-    x = (fnl - snl)*2;
-    while (x-- > 0)
-        printf(" ");
-
-    for (i = 0; i < snl; i++)
-        printf("%c ", sn[i]);
-
-    printf("\n");
-    for (i = 0; i < resl; i++)
-        printf("%c ", res[i]);
-
-}
 
 
-int main()
-{
-    int fns, sns;
-    char* fn = (char*) malloc(sizeof(char)*SIZE);
-    char* sn = (char*) malloc(sizeof(char)*SIZE);
-    char* res = (char*) malloc(sizeof(char)*SIZE);
+int main() {
+	int fns, sns;
+	char* fn = (char*) malloc(sizeof(char)*SIZE);
+	char* sn = (char*) malloc(sizeof(char)*SIZE);
+	char* res = (char*) malloc(sizeof(char)*SIZE);
 
-    scanf("%s%s", fn, sn);
+	scanf("%s%s", fn, sn);
 
-    res = Addition(fn, sn);
+	res = Extraction(fn, sn);
 
-    Show(fn, sn, res);
+	Show(fn, sn, res);
 
-    return 0;
+	return 0;
 }
